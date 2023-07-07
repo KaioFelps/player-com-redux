7ac3f49e3ea0cd20e545f21ce95b6e445adf74fd
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-07-07 17:02:00 -0300

    remove tudo

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,28 +1,14 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit"
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux"
+import { configureStore } from "@reduxjs/toolkit"
+import { TypedUseSelectorHook, useSelector } from "react-redux"
 
-const todoSlice = createSlice({
-    name: "todo",
-    initialState: ["Fazer tereré", "Estudar Redux"],
-    reducers: {
-        add: (state, action) => {
-            state.push(action.payload.newTodo)
-        }
-    }
-})
+
 
 export const store = configureStore({
-    reducer: {
-        todo: todoSlice.reducer
-    },
+    reducer: {},
 })
-
-export const {add} = todoSlice.actions
 
 // tipo do estado
 export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
 
 // adicionar tipagem para o app selector
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
-export const useAppDispatch: () => AppDispatch = useDispatch

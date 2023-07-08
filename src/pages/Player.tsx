493fc784ef7632a40413a76443fc6219a46d3d6f
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-07-07 22:10:38 -0300

    muda o titulo da pagina no navegador de acordo com a aula ativa

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -3,9 +3,17 @@ import { Header } from "../components/Header";
 import { Video } from "../components/Video";
 import { Module } from "../components/Module";
 import { useAppSelector } from "../store";
+import { useEffect } from "react";
+import { useCurrentClassAndModule } from "../store/slices/player";
 
 export function Player() {
     const modules = useAppSelector(state => state.player.course.modules)
+
+    const { currentClass } = useCurrentClassAndModule()
+
+    useEffect(() => {
+        document.title = `Redux Class: ${currentClass.title}`
+    })
 
     return (
         <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">

429cbd4e8600eb2f2a4e23f43e229585a323df71
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-07-07 22:34:20 -0300

    adiciona testes para as actions e o reducer

---
+++ src/store/slices/player.test.ts
@@ -0,0 +1,72 @@
+import { player as reducer, play, next } from "./player"
+import { describe, test, expect } from "vitest"
+
+const exampleInitialState = {
+    course: {
+        modules: [
+          {
+            id: '1',
+            title: 'Iniciando com React',
+            classes: [
+              { id: 'MNR0egvK_oQ', title: 'CSS Modules', duration: '13:45' },
+              { id: 'w-DW4DhDfcw', title: 'Estilização do Post', duration: '10:05' },
+            ],
+          },
+          {
+            id: '2',
+            title: 'Estrutura da aplicação',
+            classes: [
+              { id: 'gE48FQXRZ_o', title: 'Componente: Comment', duration: '13:45' },
+              { id: 'Ng_Vk4tBl0g', title: 'Responsividade', duration: '10:05' },
+            ],
+          },
+        ],
+    },
+    currentModuleIndex: 0,
+    currentClassIndex: 0,
+}
+
+describe("player slice", () => {
+    test("if it can play", () => {
+        const state = reducer(exampleInitialState, play([1, 1]))
+        
+        expect(state).toEqual(expect.objectContaining({
+            currentModuleIndex: 1,
+            currentClassIndex: 1,
+        }))
+    })
+
+    test("if it will play next video automatically", () => {
+        const state = reducer(exampleInitialState, next())
+
+        expect(state).toEqual(expect.objectContaining({
+            currentModuleIndex: 0,
+            currentClassIndex: 1,
+        }))
+    })
+
+    test("if it will jump to next module when the current module classes are over", () => {
+        const state = reducer({
+            ...exampleInitialState,
+            currentClassIndex: 1,
+        }, next())
+
+        expect(state).toEqual(expect.objectContaining({
+            currentModuleIndex: 1,
+            currentClassIndex: 0,
+        }))
+    })
+
+    test("if it won't change the current module nor current class if there is no next class/module", () => {
+        const state = reducer({
+            ...exampleInitialState,
+            currentClassIndex: 1,
+            currentModuleIndex: 1,
+        }, next())
+
+        expect(state).toEqual(expect.objectContaining({
+            currentModuleIndex: 1,
+            currentClassIndex: 1,
+        }))
+    })
+})
